d7ef3edc9f483f20d9ccc0b0753ca59f9d729855
Abd. Rauf Jauhari Izza Mazzidan <raufmazzidan@gmail.com>
2022-07-23 12:17:06 +0700

    ice cream sample

---
+++ src/main/java/binus/factorymethod/IceCream.java
@@ -0,0 +1,12 @@
+package binus.factorymethod;
+
+/**
+ *
+ * @author raufmazzidan
+ */
+public interface IceCream {
+
+    void setToppings(String toppings);
+
+    void prepare();
+}
---
+++ src/main/java/binus/factorymethod/IceCreamFactory.java
@@ -0,0 +1,20 @@
+package binus.factorymethod;
+
+/**
+ *
+ * @author raufmazzidan
+ */
+public class IceCreamFactory {
+
+    public static IceCream getInstance(String type) {
+        switch (type) {
+            case "1":
+                return new VanillaIceCream();
+            case "2":
+                return new StrawberryIceCream();
+            case "3":
+                return new ChocolateIceCream();
+        }
+        return null;
+    }
+}
